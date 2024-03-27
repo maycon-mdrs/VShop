bd2b9d54e035b048eb77a5c8a93ef4caacbd2234
leodavidbr <rbdivadoel@gmail.com>
2024-03-27 14:00:43 -0300

    test for categoriesController

---
+++ Test/CategoryControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VShop.ProductApi.Controllers;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Models;
+using VShop.ProductApi.Services;
+
+namespace Test
+{
+    public class CategoryControllerTest
+    {
+        private Mock<ICategoryService> _categoryServiceMock = null;
+
+        public CategoryControllerTest()
+        {
+            if(_categoryServiceMock == null)
+            {
+                _categoryServiceMock = new Mock<ICategoryService>();
+            }
+        }
+
+        [Fact]
+        public async void GetTest()
+        {
+            // arrange
+            var category = GetSampleCategory();
+            _categoryServiceMock.Setup(x => x.GetCategories()).ReturnsAsync(GetSampleCategory);
+
+            var controller = new CategoriesController(_categoryServiceMock.Object);
+
+            // act
+            var actionResult = await controller.Get();
+            var result = actionResult.Result as OkObjectResult;
+            var actual = result as IEnumerable<CategoryDTO>;
+
+
+            // assert
+            // Assert.IsType<IEnumerable<CategoryDTO>>(actual);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(result);
+            // Assert.NotNull(actual);
+            //Assert.Equal(GetSampleCategory().Count, actual.Count());
+
+        }
+
+
+        private List<CategoryDTO> GetSampleCategory()
+        {
+            CategoryDTO category = new CategoryDTO();
+            category.Name = "Test";
+            category.CategoryId = 15;
+
+            return new List<CategoryDTO> { category };
+        }
+    }
+}
---
+++ Test/CategoryServiceTest.cs
@@ -0,0 +1,15 @@
+using Moq;
+using VShop.ProductApi.Repositories;
+
+namespace Test
+{
+    public class CategoryServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var categoryRepositoryMock = new Mock<CategoryRepository>();
+
+        }
+    }
+}
